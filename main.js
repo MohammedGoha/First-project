c35a9faef5d20dd6db4b8bd98537099a50e07729
Your GitHub Username <hamadaemamemam1@gmail.com>
2025-04-30 03:34:46 +0300

    final fixing

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let exit = document.getElementById("exitOfHeader");
 let f = function () {
   menubarIcon.classList.toggle("show");
+  exit.classList.toggle("show");
 };
 let menubarIcon = document.getElementById("iconmenu");
 menubarIcon.onclick = f;
